c0308e7d7a45f7c043f93c9d61e617c27654a051
Frederic20021 <frederic123.bf@gmail.com>
2025-12-21 18:17:30 +0900

    "added youtube and insta link"

--- app/company/page.tsx
+++ app/company/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "../components/sections/hero";
 import { companyInfo } from "../constants/heroText";
 import Maps from "../components/company/maps";
+import YoutubeSection from "../components/company/YoutubeSection";
 import ContactButton from "../components/ui/contactButton";
 import Info from "../components/company/info";
 import CEOMessage from "../components/company/CEOMessage";
@@ -8,6 +9,7 @@ import CEOMessage from "../components/company/CEOMessage";
 export default function page() {
     return <>
         <HeroSection heroText={{ title: companyInfo.title }} />
+        <YoutubeSection />
         <CEOMessage />
         <Info />
         <Maps />
