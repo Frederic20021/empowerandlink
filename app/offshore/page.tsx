41f53a74b360e1f9a5171536a14f9ebae138f478
Frederic20021 <frederic123.bf@gmail.com>
2026-04-18 11:09:23 +0900

    "added the AI part"

--- app/offshore/page.tsx
+++ app/offshore/page.tsx
@@ -1,11 +1,13 @@
 import Contact from "@/app/components/offshore/Contact";
 import Pitch from "@/app/components/offshore/Pitch";
 import PricePlan from "../components/offshore/PricePlan";
+import AIinterview from "../components/offshore/AIinterview";
 
 export default function page() {
     return <>
         <Pitch />
         <PricePlan />
+        <AIinterview />
         <Contact />
     </>;
 }
